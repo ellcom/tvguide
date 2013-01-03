c996446c03f160c01d6624f3f0c13b268f130e57
Elliot Adderton <elliot@elliots-mbp-w.scooter>
2013-01-03 17:50:20 +0000

    Initial Commit

---
+++ Tv Guide/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tv Guide
+//
+//  Created by Elliot Adderton on 03/01/2013.
+//  Copyright (c) 2013 Elliot Adderton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
